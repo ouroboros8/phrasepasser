799fd754488c8aa9a9e7758ca36c0328579c8889
D Strong <dstrong@glyx.co.uk>
2014-12-26 18:13:37 +0000

    Write function to parse space seperated input files.

---
+++ parse.py
@@ -0,0 +1,14 @@
+'''
+Module for parsing wordlists from various sources into a python set.
+'''
+
+def parse_file(filename):
+    '''
+    Parse a file of whitespace-seperated words, which may span multiple
+    lines.
+    '''
+    words = set()
+    with open(filename, 'r') as handle:
+        for line in handle:
+            words = words | {word for word in line.split()}
+    return words
