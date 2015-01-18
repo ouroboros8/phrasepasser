e40f1d7a27ac2ca1882ef7402396b16e2066fae1
Daniel Strong <dstrong@glyx.co.uk>
2015-01-18 12:35:57 +0000

    Added tests

---
+++ tests.py
@@ -0,0 +1,24 @@
+import unittest
+
+from parse import str_parse
+
+class ParsingTests(unittest.TestCase):
+
+    def setUp(self):
+        test_string = '''
+                      one two three four five six seven eight nine
+                      ten eleven twelve thirteen fourteen fifteen
+                      sixteen seventeen eighteen nineteen twenty
+                      twentyone twentytwo twentythree twentyfour
+                      twentyfive
+                      '''
+        test_string_repetitions = '''
+                                  sadsa wijfi 2ei8 eban beeelll qweqw
+                                  qworw qweqw qweqw weedle tanda
+                                  partridge fandom qweqw qweqw qweqw
+                                  qweqw qweqw qweqw qweqw qweqw qweqw
+                                  qweqw qweqw qweqw qweqw qweqw
+                                  '''
+
+    def test_parse_string(self):
+        pass
