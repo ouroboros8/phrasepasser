1f428867bc287926acc25ded76e84b8d6d50dca7
Daniel Strong <dstrong@glyx.co.uk>
2015-01-18 16:12:41 +0000

    Improved file parser

--- parse.py
+++ parse.py
@@ -10,8 +10,6 @@ def parse_file(filename):
     Parse a file of whitespace-seperated words, which may span multiple
     lines.
     '''
-    words = set()
     with open(filename, 'r') as handle:
-        for line in handle:
-            words = words | {word for word in line.split()}
-    return words
+        content = handle.read().replace('\n', ' ')
+    return parse_string(content)
