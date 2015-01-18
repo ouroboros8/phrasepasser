ee26e60c0a6fbec6c52aa6b3ac9d13b271c1e7db
Daniel Strong <dstrong@glyx.co.uk>
2015-01-18 15:10:01 +0000

    Minor test refactor

--- tests.py
+++ tests.py
@@ -1,53 +1,62 @@
 from unittest import TestCase
 import os
 
-from parse import parse_string
+from parse import parse_string, parse_file
 
 class ParsingTests(TestCase):
 
     def setUp(self):
         # Create test strings
-        test_string = '''
-                      one two three four five six seven eight nine
-                      ten eleven twelve thirteen fourteen fifteen
-                      sixteen seventeen eighteen nineteen twenty
-                      twentyone twentytwo twentythree twentyfour
-                      twentyfive
-                      '''
-        test_string_repetitions = '''
-                                  sadsa wijfi 2ei8 eban beeelll
-                                  qweqw qworw qweqw qweqw weedle
-                                  tanda partridge fandom qweqw
-                                  qweqw qweqw qweqw qweqw qweqw
-                                  qweqw qweqw qweqw qweqw qweqw
-                                  qweqw qweqw qweqw
-                                  '''
-        self.test_strings = [test_string, test_string_repetitions]
+        simple_test = {'filename': 'test',
+                       'content':
+                           '''
+                           one two three four five six seven eight nine
+                           ten eleven twelve thirteen fourteen fifteen
+                           sixteen seventeen eighteen nineteen twenty
+                           twentyone twentytwo twentythree twentyfour
+                           twentyfive
+                           '''
+                      }
+
+        repetition_test = {'filename': 'reptest',
+                           'content':
+                               '''
+                               sadsa wijfi 2ei8 eban beeelll
+                               qweqw qworw qweqw qweqw weedle
+                               tanda partridge fandom qweqw
+                               qweqw qweqw qweqw qweqw qweqw
+                               qweqw qweqw qweqw qweqw qweqw
+                               qweqw qweqw qweqw
+                               '''
+                          }
+        
+        self.tests = [simple_test, repetition_test]
 
         # Crate test files
-        with open('test', 'w') as handle:
-            handle.write(test_string)
-        with open('test_rep', 'w') as handle:
-            handle.write(test_string_repetitions)
-
-        self.test_files = ['test', 'test_rep']
+        for test in self.tests:
+            filename = test['filename']
+            content = test['content']
+            with open(filename, 'w') as handle:
+                handle.write(content)
 
     def tearDown(self):
-        for filename in self.test_files:
+        filenames = [ test['filename'] for test in self.tests]
+        for filename in filenames:
             os.remove(filename)
 
     def test_parse_string(self):
-        for string in self.test_strings:
+        strings = [ test['content'] for test in self.tests ]
+        for string in strings:
             word_set = parse_string(string)
 
             for word in str.split(string):
                 self.assertIn(word, word_set)
 
     def test_parse_file(self):
-        for filename in self.test_files:
-            with open(filename, 'r') as handle:
-                string = handle.read().replace('\n', ' ')
-            word_set = parse_string(string)
+        for test in self.tests:
+            filename = test['filename']
+            content = test['content']
+            word_set = parse_file(filename)
 
-            for word in str.split(string):
+            for word in str.split(content):
                 self.assertIn(word, word_set)
