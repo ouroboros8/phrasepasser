77f0d3ac9508fac4164d981342af9d64cbe15f79
Daniel Strong <dstrong@glyx.co.uk>
2015-08-24 12:51:44 +0100

    Simple working command.

--- command.py
+++ command.py
@@ -2,17 +2,18 @@ import sys
 import argparse
 import generate
 
-def parse_wordlist(wordlist):
-    return wordlist.split()
+def parse_wordlist(wordlists):
+    return ' '.join([wordlist.read() for wordlist in wordlists]
+                   ).split()
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Generate a passphrase"
                                                  " from a wordlist.")
-    parser.add_argument('wordlist', nargs='*',
+    parser.add_argument('wordlists', nargs='*',
                         type=argparse.FileType('r'),
-                        help="file of whitespace-seperated words used "
-                             " to generate the passphrase",
+                        help="file(s) of whitespace-seperated words "
+                        " used to generate the passphrase",
                         default=sys.stdin)
 
     parser.add_argument('-l', metavar='length', type=int, default=6,
@@ -20,6 +21,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    sys.stdout.write(generate.gen_passphrase(
-        parse_wordlist(args.wordlist),
-        args.length))
+    print(generate.gen_passphrase(
+        parse_wordlist(args.wordlists),
+        args.l))
